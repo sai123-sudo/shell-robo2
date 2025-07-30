bffa68ce6389829775f50d41b434ee11f6aa9790
sai123-sudo <ls0122207@gmail.com>
2025-07-30 21:18:00 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z01776572PGDSVKCIPZD0" # replace with your ZONE ID
 DOMAIN_NAME="vara84s.site"
 
-for instance in $@
+for instance in ${INSTANCES[@]}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0af15763a7cc97cac --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
