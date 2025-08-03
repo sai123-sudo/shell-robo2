c5fa9c28113639832c3d9989b06f07677ece1db4
sai123-sudo <ls0122207@gmail.com>
2025-08-03 23:11:47 +0530

    hello worls started

--- redis.sh
+++ redis.sh
@@ -42,16 +42,16 @@ VALIDATE $? "Enabling Redis:7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
-# sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-# VALIDATE $? "Edited redis.conf to accept remote connections"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
 
-# systemctl enable redis &>>$LOG_FILE
-# VALIDATE $? "Enabling Redis"
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
 
-# systemctl start redis  &>>$LOG_FILE
-# VALIDATE $? "Started Redis"
+systemctl start redis  &>>$LOG_FILE
+VALIDATE $? "Started Redis"
 
-# END_TIME=$(date +%s)
-# TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-# echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
