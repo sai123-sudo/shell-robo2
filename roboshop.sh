7e465cd444e9de88c2ab4452e458ee126aad6be3
sai123-sudo <ls0122207@gmail.com>
2025-08-04 10:56:05 +0530

    hello worls started

--- roboshop.sh
+++ roboshop.sh
@@ -19,8 +19,8 @@ do
     fi
     echo "$instance IP address: $IP"
 
-     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
+     aws route53 change-resource-record-sets
+    --hosted-zone-id $ZONE_ID
     --change-batch '
     {
         "Comment": "Creating or Updating a record set for cognito endpoint"
